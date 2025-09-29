6b5b4dfe574ad33231fe1373055d5a3c3cc2e56a
breechelugui <brendachelugui@gmail.com>
2025-09-29 19:02:13 +0300

    initial commit

--- main.js
+++ main.js
@@ -4,7 +4,39 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+// Grab modal and message span
+const modal = document.getElementById("modal")
+const modalMessage = document.getElementById("modal-message")
 
+// Wait until DOM is loaded
+document.addEventListener("DOMContentLoaded", () => {
+  // Get all heart elements
+  const hearts = document.querySelectorAll(".like-glyph")
+
+  hearts.forEach(heart => {
+    heart.addEventListener("click", () => {
+      mimicServerCall()
+        .then(() => {
+          if (heart.textContent === EMPTY_HEART) {
+            heart.textContent = FULL_HEART
+            heart.classList.add("activated-heart")
+          } else {
+            heart.textContent = EMPTY_HEART
+            heart.classList.remove("activated-heart")
+          }
+        })
+        .catch((error) => {
+          modal.classList.remove("hidden")
+          modalMessage.textContent = error
+
+          // Hide modal again after 3 seconds
+          setTimeout(() => {
+            modal.classList.add("hidden")
+          }, 3000)
+        })
+    })
+  })
+})
 
 
 //------------------------------------------------------------------------------
